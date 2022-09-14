75441819135531524d4115d4080b5738b0ea6044
Shanky43 <shankarshanky43@gmail.com;git config --global user.name Shanky43git config --global user.email shankarshanky43@gmail.com>
2022-09-15 00:21:45 +0530

    login logic

--- login.js
+++ login.js
@@ -1,2 +1,12 @@
 let reg_email_id = abc@gmail.com;
-let reg_pass_wrd = abc@123#;
+let reg_pass_wrd = abc@123#; 
+
+let entered_email_id=abc@gmail.com;
+let entered_pass_wrd = abc@123#;
+if(reg_email_id == entered_email_id){
+if(reg_pass_wrd == entered_pass_wrd){
+console.log("Login is Successfull");
+}else{
+console.log("Login is unsuccessfull");
+}
+}
